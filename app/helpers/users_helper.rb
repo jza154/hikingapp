83677dd777bdf13364badce24437c657e5069474
James <Jza154@sfu.ca>
2016-07-12 06:05:08 +0000

    image size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=https%3A%2F%2Fwww.gravatar.com%2Favatar%2Faf0aa7a8aa4f4cd528a0d663379c99ac"
+    default_url = "https%3A%2F%2Fwww.gravatar.com%2Favatar%2Faf0aa7a8aa4f4cd528a0d663379c99ac?s=48"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=48&d=#{default_url}"
     image_tag(gravatar_url, alt: user.email, class: "gravatar") # may change to username once it's added
   end
   
