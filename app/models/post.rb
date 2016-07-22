6f0bb6cc4763b696a7e8ff6a12402d53e0d0137d
James <Jza154@sfu.ca>
2016-07-22 06:32:04 +0000

    link user to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < ActiveRecord::Base
-
-  # belongs_to :user
+  belongs_to :user
+  
   def self.search(search)
     where("title LIKE ?", "%#{search}%") 
     where("content LIKE ?", "%#{search}%")
   end
-
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-#   has_many :posts
+  has_many :posts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
- def self.search(search)
-  where("name LIKE ?", "%#{search}%") 
-end
+  def self.search(search)
+      where("name LIKE ?", "%#{search}%") 
+  end
 end
