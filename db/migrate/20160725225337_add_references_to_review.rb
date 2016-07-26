c68fe5fca8ca9f4fbea19cdbb8a93df40e4145e1
Nina <nina.xh.q@gmail.com>
2016-07-26 02:01:18 +0000

    references review

--- db/migrate/20160725225337_add_references_to_review.rb
+++ db/migrate/20160725225337_add_references_to_review.rb
@@ -3,7 +3,7 @@ class AddReferencesToReview < ActiveRecord::Migration
     add_column :reviews, :reviewer_id, :integer
     add_column :reviews, :receiver_id, :integer
     
-    add_foreign_key :reviews, :user, column: :reviewer_id
-    add_foreign_key :reviews, :user, column: :receiver_id
+    add_foreign_key :reviews, :users, column: :reviewer_id
+    add_foreign_key :reviews, :users, column: :receiver_id
   end
 end
